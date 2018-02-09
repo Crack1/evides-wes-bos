fff348eab34c47e82bf6eb7a5c25d8847894b4d5
Erwin <erwinvides@hotmail.com>
2018-02-08 22:07:10 -0600

    The video 13 was completed

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,30 @@
+const mongoose = require('mongoose')
+const Store = mongoose.model('Store')
+
+
 exports.homePage = (req, res) => {
   res.render('index')
 }
 
 exports.addStore = (req, res) => {
-  res.render('editStore', {title: 'Add Store'})
+  res.render('editStore', {
+    title: 'Add Store'
+  })
 }
 
-exports.createStore = (req, res) => {
-  res.json(req.body)
+exports.createStore = async (req, res) => {
+  // let store = new Store(req.body)
+  // await store.save()
+  let store = await (new Store(req.body)).save()
+
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a Review`)
+  res.redirect(`/store/${store.slug}`)
+
+}
+exports.getStores = async (req, res) => {
+  const stores = await Store.find()
+  res.render('stores', {
+    title: 'Stores',
+    stores: stores
+  })
 }
